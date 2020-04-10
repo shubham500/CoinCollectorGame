fda204e79d65a9f91dbee4e4d49e6cbbc77475a9
shubham500 <shubhampotekar96@gmail.com>
2020-04-11 00:04:27 +0530

    Initial commit

---
+++ android/src/com/mygame/coincollector/game/AndroidLauncher.java
@@ -0,0 +1,19 @@
+package com.mygame.coincollector.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygame.coincollector.game.CoinCollector;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CoinCollector(), config);
+	}
+}
+
